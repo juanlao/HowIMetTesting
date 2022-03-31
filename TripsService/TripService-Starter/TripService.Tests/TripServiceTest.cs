841c67ec0a2d32b44caaf3c16f6bfec6d8345d69
Juan María Laó Ramos <jlao@plainconcepts.com>
2022-03-31 20:44:01 +0200

    Initial commit

---
+++ TripsService/TripService-Starter/TripService.Tests/TripServiceTest.cs
@@ -0,0 +1,16 @@
+﻿using ContosoTrips.Exception;
+using ContosoTrips.Trip;
+using ContosoTrips.User;
+using FluentAssertions;
+using System;
+using System.Collections.Generic;
+using Xunit;
+using static FluentAssertions.FluentActions;
+
+namespace TripServiceTests
+{
+    public class trip_service_should
+    {
+
+    }
+}
